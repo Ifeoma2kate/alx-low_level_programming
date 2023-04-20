3bce3f1c31ad72b65ce9a4eae96249d028f71fcb
Ifeoma2kate <ifeomaokpala15@gmail.com>
2023-04-20 16:15:22 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
 /**
- * main -Entry point
- *
- * Return Always 0n(Success)
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle
-")	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 	
