eda6fda9a569f6e54882ffb927cfa4afa15a9335
Ifeoma2kate <ifeomaokpala15@gmail.com>
2023-04-26 10:19:15 -0700

    8-24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * jack_baver - Prints every minute of the day of Jack Baver
+ * jack_bauer - Prints every minute of the day of Jack Bauer
  * Return: ...
  */
 
-void jack_baver(void)
+void jack_bauer(void)
 
 {
 	int a, b, c, d;
-	
+
 	for (a = 0; a <= 2; a++)
 	{
 	for (b = 0; b <= 9; b++)
@@ -24,7 +24,7 @@ void jack_baver(void)
 	_putchar(b + '0');
 	_putchar(58);
 	_putchar(c + '0');
-	_putchar(d + '0');
+	_putchar(d  + '0');
 	_putchar('\n');
 	}
 	}
