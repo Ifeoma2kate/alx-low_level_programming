de6db327b1fdb4cfc44830ce682c44c8cf9786c5
Ifeoma2kate <ifeomaokpala15@gmail.com>
2023-04-27 23:22:12 -0700

    0-is upper

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 
 /**
- * main -Entry point
- *
- * Return: Always l (Success)
+ * main - A c program that prints a line to the standard error
+ * Return: l (Success)
  * /
  int main(void)
  {
- 	write(2,
+ 	fput("and that piece of art is useful" - Dora Korpar, 2015-10-19
 	"and that piece of art is useful\" - Dora Korpar, 2015-10-19n" . 59):
 	return (1);
 }
